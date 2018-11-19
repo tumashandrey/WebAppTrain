c8d6c2f889300c889474d1788937962682fcc34d
tumashandrey <tumashandrey96@gmail.com>
2018-11-19 18:20:20 +0300

    Routes controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :trains
   resources :railway_stations
+  resources :routes
   get 'home/index'
   root 'home#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
