763dc7958f0bf54642d31e9c0bb9e0d6a8ad7cf5
tumashandrey <tumashandrey96@gmail.com>
2018-11-19 15:14:29 +0300

    commit add Train and Home page

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_19_114916) do
+ActiveRecord::Schema.define(version: 2018_11_19_120200) do
 
   create_table "railway_stations", force: :cascade do |t|
     t.string "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "trains", force: :cascade do |t|
+    t.integer "number"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
